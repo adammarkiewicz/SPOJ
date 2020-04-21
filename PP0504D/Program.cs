9e31ab3b05d819c26b133e07324694d506a64a1b
Adam Markiewicz <adam.markiewicz@wp.pl>
2020-04-21 21:31:09 +0200

    PP0504D - Reprezentacja liczb typu float

---
+++ PP0504D/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace PP0504D
+{
+    class Program
+    {
+        static void Main()
+        {
+            short testsCount = Convert.ToInt16(Console.ReadLine());
+            StringBuilder output = new StringBuilder();
+
+            while (testsCount-- > 0)
+            {
+                float x = float.Parse(Console.ReadLine());
+                byte[] bytes = BitConverter.GetBytes(x);
+                StringBuilder hex = new StringBuilder(bytes.Length * 2);
+
+                for (int i = bytes.Length - 1; i >= 0; i--)
+                {
+                    hex.AppendFormat("{0:x} ", bytes[i]);
+                }
+
+                output.Append(hex + Environment.NewLine);
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ PP0504D/README.md
@@ -0,0 +1,3 @@
+PP0504D - Reprezentacja liczb typu float
+
+https://pl.spoj.com/problems/PP0504D/
