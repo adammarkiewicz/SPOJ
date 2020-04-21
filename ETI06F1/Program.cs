40dba47ec25779174bfe1b23980b7c80273329dd
Adam Markiewicz <adam.markiewicz@wp.pl>
2020-04-22 01:00:29 +0200

    ETI06F1 - Pole pewnego koła

---
+++ ETI06F1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ETI06F1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double[] array = Console.ReadLine().Split(' ').Select(Double.Parse).ToArray();
+
+            double r = array[0];
+            double d = array[1];
+
+            Console.WriteLine(Math.PI * (r * r - d * d / 4));
+        }
+    }
+}
---
+++ ETI06F1/README.md
@@ -0,0 +1,3 @@
+ETI06F1 - Pole pewnego koła
+
+https://pl.spoj.com/problems/ETI06F1/
