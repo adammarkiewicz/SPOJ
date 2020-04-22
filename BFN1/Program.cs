7b70a12917f6c45bffe1aaca62b4f00fb6c74ad8
Adam Markiewicz <adam.markiewicz@wp.pl>
2020-04-22 02:10:30 +0200

    BFN1 - Zabawne Dodawanie Piotrusia

---
+++ BFN1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BFN1
+{
+    class Program
+    {
+        static void Main()
+        {
+            short testsCount = Convert.ToInt16(Console.ReadLine());
+            string number;
+            int additionsCounter;
+
+            for (short c = 0; c < testsCount; c++)
+            {
+                number = Console.ReadLine();
+                additionsCounter = 0;
+
+                while (number != ReverseString(number))
+                {
+                    number = (Convert.ToInt16(number) + Convert.ToInt16(ReverseString(number))).ToString();
+                    additionsCounter++;
+                }
+
+                Console.WriteLine(number + " " + additionsCounter);
+            }
+        }
+
+        private static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
---
+++ BFN1/README.md
@@ -0,0 +1,3 @@
+BFN1 - Zabawne Dodawanie Piotrusia
+
+https://pl.spoj.com/problems/BFN1/
