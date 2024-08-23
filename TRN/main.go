4ebc9334387430424b12de48a207203b26f6d576
Adam Markiewicz <adam.markiewicz@wp.pl>
2024-08-23 21:27:54 +0200

    TRN - Transponowanie macierzy

--- TRN/main.go
+++ TRN/main.go
@@ -10,20 +10,17 @@ func trn() {
 	var i, j int
 	fmt.Scanln(&i, &j)
 
-	tranMx := make([][]int, j)
-	for y := 0; y < j; y++ {
-		tranMx[y] = make([]int, i)
-	}
+	mx := make([]int, i*j)
 
 	for x := 0; x < i; x++ {
 		for y := 0; y < j; y++ {
-			fmt.Scan(&tranMx[y][x])
+			fmt.Scan(&mx[j*x+y])
 		}
 	}
 
 	for y := 0; y < j; y++ {
 		for x := 0; x < i; x++ {
-			fmt.Print(tranMx[y][x], " ")
+			fmt.Print(mx[j*x+y], " ")
 		}
 
 		fmt.Println()
